65cd64a326aae1bbdab2bd0c4cd5fcbe2fe0f932
Mohit Yadav <mohit.yadav@kredx.com>
2024-05-24 23:44:45 +0530

    Add UserMapper

---
+++ src/main/java/com/mohity/userservice/UserMapper.java
@@ -0,0 +1,13 @@
+package com.mohity.userservice;
+
+public class UserMapper {
+
+    public static UserResponse toUserResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .build();
+    }
+}
