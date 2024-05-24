0b338cae05c826b930124850ae5b25bd6521cc37
Mohit Yadav <mohit.yadav@kredx.com>
2024-05-24 23:29:12 +0530

    Update UserResponse.java

--- src/main/java/com/mohity/userservice/UserResponse.java
+++ src/main/java/com/mohity/userservice/UserResponse.java
@@ -1,9 +1,12 @@
 package com.mohity.userservice;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserResponse {
+    private Long id;
     private String name;
     private String email;
     private Role role;
